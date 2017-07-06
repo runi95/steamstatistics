94fc82078c259a311d1837d31b6c56805f43e9c8
runi95 <Runar.Serigstad@student.uib.no>
2017-07-06 18:56:39 +0200

    Added user agreement and database check for current user

--- src/main/java/com.steamstatistics/controller/Controller.java
+++ src/main/java/com.steamstatistics/controller/Controller.java
@@ -6,9 +6,11 @@ import com.steamstatistics.data.SteamProfileModel;
 import com.steamstatistics.data.SteamProfileService;
 import com.steamstatistics.services.SteamProfileHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +18,11 @@ import javax.servlet.http.HttpServletResponse;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+    @Value("${auth.baseurl}")
+    private String baseurl;
+
+    private final String userAgreement = "This website stores information about your steam profile and friends list to keep up to date, by clicking accept you agree to let us gather and store information from your steam profile. Users can delete the stored data at any given time by logging in through the Steam again and clicking the delete button. The information gathered will be public information from your steam profile which means this site can't function if your profile is on private. Click accept to continue";
+
     private final SteamOpenId steamOpenId = new SteamOpenId();
 
     @Autowired
@@ -27,18 +34,39 @@ public class Controller {
     @GetMapping(value="/")
     public String getHomepage(@CookieValue(value = "steamid", required = false) String steamid, Model model) {
         SteamProfileModel steamProfileModel = null;
-        if(steamid != null && !steamid.isEmpty())
+        SteamProfileEntity steamProfileEntity = null;
+        if(steamid != null && !steamid.isEmpty()) {
             steamProfileModel = steamProfileHandler.getProfile(steamid);
 
+            Long steamidlong = null;
+            try{
+                steamidlong = Long.parseLong(steamid);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+
+            if(steamidlong != null) {
+                steamProfileEntity = steamProfileService.get(steamidlong);
+            }
+        }
+
         model.addAttribute("steamprofile", steamProfileModel);
+        model.addAttribute("steamprofilestats", steamProfileEntity);
+        model.addAttribute("useragreement", userAgreement);
 
         return "home";
+    }
+
+    @PostMapping(value = "/")
+    public String userAgreement() {
+
+        return "redirect:/";
     }
 
     @GetMapping(value="/auth")
     public String getAuth(@CookieValue(value = "steamid", required = false) String steamid, HttpServletRequest request, HttpServletResponse response) {
         if(steamid == null || steamid.isEmpty()) {
-            String user = steamOpenId.verify("http://192.168.1.174:8080/auth", request.getParameterMap());
+            String user = steamOpenId.verify(baseurl + "/auth", request.getParameterMap());
 
             response.addCookie(new Cookie("steamid", user));
         }
@@ -48,7 +76,7 @@ public class Controller {
 
     @GetMapping(value="/trade")
     public String getTrade() {
-        return "redirect:" + steamOpenId.login("http://192.168.1.174:8080/auth");
+        return "redirect:" + steamOpenId.login(baseurl + "/auth");
     }
 
     @GetMapping(value="/login")
