a7b0cac1ccbd3500930d4c7a3f712f25ab43b1ee
runi95 <Runar.Serigstad@student.uib.no>
2017-07-06 15:23:50 +0200

    Fixed a few minor issues with cookies and redirecting auth

--- src/main/java/com.steamstatistics/controller/Controller.java
+++ src/main/java/com.steamstatistics/controller/Controller.java
@@ -3,6 +3,7 @@ package com.steamstatistics.controller;
 import com.steamstatistics.backend.SteamOpenId;
 import com.steamstatistics.data.SteamProfileEntity;
 import com.steamstatistics.data.SteamProfileModel;
+import com.steamstatistics.data.SteamProfileService;
 import com.steamstatistics.services.SteamProfileHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -20,10 +21,11 @@ public class Controller {
     @Autowired
     private SteamProfileHandler steamProfileHandler;
 
+    @Autowired
+    private SteamProfileService steamProfileService;
+
     @GetMapping(value="/")
     public String getHomepage(@CookieValue(value = "steamid", required = false) String steamid, Model model) {
-        System.out.println("steasmid: " + steamid);
-
         SteamProfileModel steamProfileModel = null;
         if(steamid != null && !steamid.isEmpty())
             steamProfileModel = steamProfileHandler.getProfile(steamid);
@@ -34,13 +36,14 @@ public class Controller {
     }
 
     @GetMapping(value="/auth")
-    public String getAuth(HttpServletRequest request, HttpServletResponse response) {
-        String user = steamOpenId.verify("http://192.168.1.174:8080/auth", request.getParameterMap());
+    public String getAuth(@CookieValue(value = "steamid", required = false) String steamid, HttpServletRequest request, HttpServletResponse response) {
+        if(steamid == null || steamid.isEmpty()) {
+            String user = steamOpenId.verify("http://192.168.1.174:8080/auth", request.getParameterMap());
 
-        System.out.println("setting cookie to: " + user);
-        response.addCookie(new Cookie("steamid", user));
+            response.addCookie(new Cookie("steamid", user));
+        }
 
-        return "/";
+        return "redirect:/";
     }
 
     @GetMapping(value="/trade")
