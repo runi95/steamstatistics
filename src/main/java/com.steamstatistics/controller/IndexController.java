2d2eadae365896de23cc662383e9213778300a11
runar <thegreatandpowerfulmetal@gmail.com>
2017-07-16 00:17:41 +0200

    Fixed compilation error

--- src/main/java/com.steamstatistics/controller/IndexController.java
+++ src/main/java/com.steamstatistics/controller/IndexController.java
@@ -7,6 +7,7 @@ import com.steamstatistics.data.SteamProfileService;
 import com.steamstatistics.steamapi.SteamAPICaller;
 import com.steamstatistics.steamapi.SteamFriends;
 import com.steamstatistics.steamapi.SteamHandler;
+import com.steamstatistics.steamapi.TimeService;
 import com.steamstatistics.userauth.SteamUserDetailsService;
 import com.steamstatistics.userauth.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,8 @@ public class IndexController {
     SteamAPICaller steamAPICaller = new SteamAPICaller();
 
     SteamHandler steamHandler = new SteamHandler();
+
+    TimeService timeService = new TimeService();
 
     private final String userAgreement = "This website stores information about your steam profile and friends list to keep up to date, by clicking accept you agree to let us gather and store information from your steam profile. Users can delete the stored data at any given time by logging in through the Steam again and clicking the delete button. The information gathered will be public information from your steam profile which means this site can't function if your profile is on private. Click accept to continue";
 
@@ -84,7 +87,7 @@ public class IndexController {
     public String acceptTerms(Principal principal) {
         if (principal != null) {
             UserPrincipal userPrincipal = steamUserDetailsService.loadUserByUsername(principal.getName());
-            SteamProfileEntity steamProfileEntity = new SteamProfileEntity();
+            SteamProfileEntity steamProfileEntity = new SteamProfileEntity(timeService.getCurrentUnixTime());
             steamProfileEntity.setSteamid(Long.parseLong(userPrincipal.getSteamId()));
             steamProfileEntity.setAuthtoken(userPrincipal.getUsername());
             steamProfileService.save(steamProfileEntity);
