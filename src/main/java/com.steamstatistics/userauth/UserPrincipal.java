bce83b4456d0aa72f0cf66687de224512fd96348
runar <thegreatandpowerfulmetal@gmail.com>
2017-11-03 19:01:23 +0100

    Updated javadocs

--- src/main/java/com.steamstatistics/userauth/UserPrincipal.java
+++ src/main/java/com.steamstatistics/userauth/UserPrincipal.java
@@ -33,21 +33,37 @@ public class UserPrincipal implements UserDetails {
 
     // TODO: Have a look at the boolean checks
 
+    /**
+     * Indicates whether the user's account has expired. An expired account cannot be authenticated.
+     * @return true if the user's account is valid (ie non-expired), false if no longer valid (ie expired)
+     */
     public boolean isAccountNonExpired() {
         // Always true for now, might change in future updates!
         return true;
     }
 
+    /**
+     * Indicates whether the user is locked or unlocked. A locked user cannot be authenticated.
+     * @return true if the user is not locked, false otherwise
+     */
     public boolean isAccountNonLocked() {
         // Always true for now, might change in future updates!
         return true;
     }
 
+    /**
+     * Indicates whether the user's credentials (password) has expired. Expired credentials prevent authentication.
+     * @return true if the user's credentials are valid (ie non-expired), false if no longer valid (ie expired)
+     */
     public boolean isCredentialsNonExpired() {
         // Always true as we don't have a password
         return true;
     }
 
+    /**
+     * Indicates whether the user is enabled or disabled. A disabled user cannot be authenticated.
+     * @return true if the user is enabled, false otherwise
+     */
     public boolean isEnabled() {
         // Always true for now, might change in future updates!
         return true;
