23c1a30901418b8a84fdd45c84895a3d81bc9976
runar <thegreatandpowerfulmetal@gmail.com>
2017-11-10 21:59:18 +0100

    Added hourly updates to the database

---
+++ src/main/java/com.steamstatistics/backend/DatabaseUpdater.java
@@ -0,0 +1,75 @@
+package com.steamstatistics.backend;
+
+import com.steamstatistics.data.SteamFriendEntity;
+import com.steamstatistics.data.SteamFriendService;
+import com.steamstatistics.steamapi.SteamAPICaller;
+import com.steamstatistics.steamapi.TimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Component
+public class DatabaseUpdater extends Thread {
+    private static final int sleepTime = 3600;  // Hourly
+    private boolean updating = true;
+
+    @Autowired
+    SteamFriendService steamFriendService;
+
+    @Autowired
+    TimeService timeService;
+
+    @Autowired
+    SteamAPICaller steamAPICaller;
+
+    @Autowired
+    SteamOpenIdConfig steamOpenIdConfig;
+
+    public boolean isUpdating() {
+        return updating;
+    }
+
+    public void steUpdate(boolean updating) {
+        this.updating = updating;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        start();
+    }
+
+    @Override
+    public void run() {
+        while(updating) {
+            LocalDateTime localDateTime = timeService.getLocalDateTimeFromUnix(timeService.getCurrentUnixTime());
+            System.out.println(localDateTime.getYear() + "-" + localDateTime.getMonthValue() + "-" + localDateTime.getDayOfMonth() + " " + localDateTime.getHour() + ":" + localDateTime.getMinute() + ":" + localDateTime.getSecond() + " INFO --- [\tDatabaseUpdater");
+            Iterable<SteamFriendEntity> getAll = steamFriendService.getAll();
+            Iterator<SteamFriendEntity> iterator = getAll.iterator();
+
+            List<Long> steamFriendsToUpdate = new ArrayList<>();
+            while (iterator.hasNext()) {
+                SteamFriendEntity friend = iterator.next();
+
+                long lastUpdated = friend.getUpdatetime();
+                long yesterday = timeService.getYesterdayUnixTime();
+
+                if (lastUpdated < yesterday) {
+                    steamFriendsToUpdate.add(friend.getSteamid());
+                }
+            }
+
+            steamAPICaller.getPlayerSummaries(steamOpenIdConfig.getClientSecret(), steamFriendsToUpdate);
+
+            try {
+                sleep(sleepTime * 100);
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
