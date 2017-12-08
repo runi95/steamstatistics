425b0fe05730a6dfedf5c766a753710e31eb1f96
runar <thegreatandpowerfulmetal@gmail.com>
2017-12-08 21:32:22 +0100

    Had to specify with a @Table annotation when using postgresql

--- src/main/java/com.steamstatistics/userauth/User.java
+++ src/main/java/com.steamstatistics/userauth/User.java
@@ -3,8 +3,10 @@ package com.steamstatistics.userauth;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
